ce1d2084931290333365e8733c9cc1d9b83fdc29
wvxbs <gabriel.ferreira7854@gmail.com>
2021-09-06 23:58:10 -0300

    write to file logic done

--- src/WriteLoginToTextFile.py
+++ src/WriteLoginToTextFile.py
@@ -1,3 +1,10 @@
+def ParseDataToWriteToFIle(User, Password):
+    ParsedData = f"user: {User} \nPassword: {Password}"
+
+    return ParsedData
+
 def WriteLoginToTextFile(User, Password):
+    with open('login.txt', 'w') as f:
+        f.write(ParseDataToWriteToFIle(User, Password))
 
     return True
